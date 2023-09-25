bcc010b5526f5495fc50ebcf02a66eb4d98edcb0
Joachim Wiberg <troglobit@gmail.com>
2023-09-25 04:12:13 +0200

    package/finit: bump to v4.4
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.3
+FINIT_VERSION = 4.4
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
 FINIT_LICENSE_FILES = LICENSE
@@ -14,8 +14,8 @@ FINIT_INSTALL_STAGING = YES
 FINIT_D = $(TARGET_DIR)/etc/finit.d
 
 # Create configure script using autoreconf when building from git
-#FINIT_VERSION = 099672f
-#FINIT_SITE = git://github.com/troglobit/finit.git
+#FINIT_VERSION = 438d6b4e638418a2a22024a3cead2f47909d72b9
+#FINIT_SITE = $(call github,troglobit,finit,$(FINIT_VERSION))
 #FINIT_AUTORECONF = YES
 #FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
 
@@ -87,6 +87,12 @@ ifeq ($(BR2_PACKAGE_FINIT_PLUGIN_RTC),y)
 FINIT_CONF_OPTS += --enable-rtc-plugin
 else
 FINIT_CONF_OPTS += --disable-rtc-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_FINIT_RTC_DATE),y)
+FINIT_CONF_OPTS += --with-rtc-date="$(BR2_PACKAGE_FINIT_RTC_DATE)"
+else
+FINIT_CONF_OPTS += --without-rtc-date
 endif
 
 ifeq ($(BR2_PACKAGE_FINIT_PLUGIN_TTY),y)
